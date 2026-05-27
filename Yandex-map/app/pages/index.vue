90e0af80c5a288c7e6c39a16ec945c6c7cdfe6b2
Александр <sania__88@mail.ru>
2026-05-27 13:32:21 +0500

    test page

--- Yandex-map/app/pages/index.vue
+++ Yandex-map/app/pages/index.vue
@@ -1,20 +1,5 @@
 <template>
   <div>
-    <YandexMap
-      :settings="{
-        location: {
-          center: [37.617644, 55.755819],
-          zoom: 10,
-        },
-      }"
-      width="100%"
-      height="500px"
-    >
-      <YandexMapDefaultSchemeLayer />
-    </YandexMap>
+    <h1>Привет, мир!</h1>
   </div>
 </template>
-
-<script setup>
-import { YandexMap, YandexMapDefaultSchemeLayer } from 'vue-yandex-maps';
-</script>
