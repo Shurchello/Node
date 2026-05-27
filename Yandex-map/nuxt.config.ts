65061b8155a89d3ceed80bed7b0775a13662d93c
Александр <sania__88@mail.ru>
2026-05-27 13:39:43 +0500

    nuxt.config.ts change

--- Yandex-map/nuxt.config.ts
+++ Yandex-map/nuxt.config.ts
@@ -3,12 +3,12 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
-    'vue-yandex-maps/nuxt', 
+    'vue-yandex-maps/nuxt',
   ],
 
   yandexMaps: {
-  apikey: process.env.NUXT_PUBLIC_YANDEX_MAPS_API_KEY,
-},
+    apikey: process.env.NUXT_PUBLIC_YANDEX_MAPS_API_KEY,
+  },
 
   devtools: {
     enabled: true
@@ -17,16 +17,26 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   routeRules: {
-  '/': { prerender: true }
-},
+    '/': { prerender: true }
+  },
 
   compatibilityDate: '2025-01-15',
-vite: {
+
+  vite: {
     ssr: {
       noExternal: ['vue-yandex-maps'],
     },
-  }, 
-  
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: '_nuxt/[name][extname]',
+          chunkFileNames: '_nuxt/[name].js',
+          entryFileNames: '_nuxt/[name].js',
+        }
+      }
+    }
+  },
+
   // @ts-ignore
   eslint: {
     config: {
@@ -37,4 +47,3 @@ vite: {
     }
   }
 })
-
